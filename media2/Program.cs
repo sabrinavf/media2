c34236d7f91fc71b63d4ae60d1629c2e1bee29fb
Sabrinavf <contactsabrinafernandes@gmail.com>
2023-11-17 19:19:36 -0300

    Adicionar arquivos de projeto.

---
+++ media2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estudo
+{
+    class media2
+    {
+        static void Main(string[] args)
+        {
+
+           double A, B, C, media;
+
+            A = Double.Parse (Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = ((A * 2) + (B * 3) + (C * 5)) / 10;
+            Console.WriteLine("MEDIA = " + media.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
